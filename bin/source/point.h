dba375b671c8655291586f921cf5441e6196e882
Marvin Janosch <marvin@janosch.koeln>
2017-11-07 18:53:25 +0100

    Ordnerumstruckturierung

---
+++ bin/source/point.h
@@ -0,0 +1,19 @@
+#ifndef POINT_H
+#define POINT_H
+
+class Point
+{
+private:
+    double x;
+    double y;
+public:
+    Point();
+    Point(double nx, double ny);
+    void setX(double nx);
+    void setY(double ny);
+    double getX();
+    double getY();
+    Point operator = (Point* np);
+};
+
+#endif // POINT_H
