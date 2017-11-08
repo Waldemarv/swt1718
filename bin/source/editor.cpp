ecefeffcff609752c6bde4191fd04074aa4efe82
sudodelC <33464395+sudodelC@users.noreply.github.com>
2017-11-08 13:28:12 +0100

    Editor changes
    Map erstellen, speichern und laden sowie die jeweiligen abfragen hinzugefügt.

--- bin/source/editor.cpp
+++ bin/source/editor.cpp
@@ -1,10 +1,10 @@
 #include "editor.h"
-#include "ui_editor.h"
-#include "map.h"
+
 Editor::Editor(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Editor)
 {
+    m = nullptr;
     ui->setupUi(this);
 }
 
@@ -13,13 +13,85 @@ Editor::~Editor()
     delete ui;
 }
 
+void Editor::createMap()
+{
+    if(m == nullptr) {
+        bool ok;
+        int x = QInputDialog::getInt(this, tr("Neue Map erstellen.."), tr("Mapgröße festlegen:"), 32, 16, 64, 1, &ok);
+        this->m =  new Map(x,x);
+    }
+    else {
+        QMessageBox::StandardButton reply;
+        reply = QMessageBox::question(this, "Alte Map speichern?", "Möchten Sie vor dem Erstellen ihrer neuen Map die vorherige Karte speichern?",
+                                      QMessageBox::Yes|QMessageBox::No|QMessageBox::Abort);
+        if (reply == QMessageBox::Yes) {
+            this->saveMap();
+            m = nullptr;
+            this->createMap();
+        }
+        else if(reply == QMessageBox::Abort) {
+
+        }
+        else {
+            m = nullptr;
+            this->createMap();
+        }
+    }
+}
+
+void Editor::saveMap()
+{
+    //Pfad zum speichern wählen
+    QString filepath = QFileDialog::getSaveFileName(
+            this,
+            tr("Dateipfad wählen..."),
+            "C:/MyMap.xml",
+            tr("Extensible Markup Language Files (*.xml)")
+);
+
+    //TODO: Standard für zu speichernde Datei festlegen (xml)
+}
+
+void Editor::loadMap()
+{
+    //Pfad für die zu ladene Map wählen
+    QString filename = QFileDialog::getOpenFileName(
+                this,
+                tr("Datei wählen..."),
+                "C:/",
+                tr("Extensible Markup Language Files (*.xml)")
+    );
+}
+
+void Editor::on_actionMap_Laden_triggered()
+{
+    this->loadMap();
+}
+
+void Editor::on_actionMap_Speichern_triggered()
+{
+    this->saveMap();
+}
+
 void Editor::on_actionNeue_Map_triggered()
 {
-    Map* m = new Map();
+    this->createMap();
 }
 
 void Editor::on_actionSchliessen_triggered()
 {
-    //TODO : Map speichern/verwerfen Fenster!
-    close();
+      //Vor dem schließen Ja/Nein/Abbrechen Abfrage
+      QMessageBox::StandardButton reply;
+      reply = QMessageBox::question(this, "Vor dem schliessen speichern?", "Möchten Sie das Projekt vor dem schließen speichern?",
+                                    QMessageBox::Yes|QMessageBox::No|QMessageBox::Abort);
+      if (reply == QMessageBox::Yes) {
+          this->saveMap();
+          QApplication::quit();
+      }
+      else if(reply == QMessageBox::Abort) {
+
+      }
+      else {
+          QApplication::quit();
+      }
 }
