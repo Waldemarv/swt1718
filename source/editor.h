fc68835ab28330c99193e954166407055d1a4386
DeSzp <dennis.szczepanski@web.de>
2017-11-15 20:13:00 +0100

    Koordinatenraster & QGraphicsGridLayout

---
+++ source/editor.h
@@ -0,0 +1,80 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+#include "map.h"
+#include "ui_editor.h"
+
+#include <QGraphicsLayout>
+#include <QGraphicsGridLayout>
+#include <QtCore>
+#include <QGraphicsItem>
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QtXml>
+#include <QtGui>
+
+#include <QMainWindow>
+
+namespace Ui {
+/*! Stellt grundlegende Funktionen zum Erstellen und bearbeiten von Maps in einer graphischen Oberfläche zur Verfügung */
+class Editor;
+}
+
+class Editor : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Editor(QWidget *parent = 0);
+    ~Editor();
+
+    void createMap();
+    void saveMap();
+    void loadMap();
+    void clearTree();
+    void deleteMap();
+
+    void updateTreeTiles();
+    void updateTreeObstacles();
+    void updateTreeSize();
+    void updateMapValues(int x, int y);
+
+    void addTreeItems();
+    void addTreeMap(double x, double y);
+
+    void addChild(QTreeWidgetItem *parent, QString name, int posX, int posY);
+
+private slots:
+    void on_actionNeue_Map_triggered();
+
+    void on_actionSchliessen_triggered();
+
+    void on_actionMap_Speichern_triggered();
+
+    void on_actionMap_Laden_triggered();
+
+    void on_straightButton_clicked();
+
+    void on_turnButton_clicked();
+
+    void on_deleteTile_clicked();
+
+    void on_staticObstacle_clicked();
+
+    void on_deleteObstacle_clicked();
+
+    void on_deleteSelectedTile_clicked();
+
+    void on_deleteSelectedObstacle_clicked();
+
+private:
+    Ui::Editor *ui;
+    Map* m;
+    QGraphicsScene *scene;
+    QTreeWidgetItem* treeRoot;
+    QTreeWidgetItem* treeObstacles;
+    QTreeWidgetItem* treeTiles;
+};
+
+#endif // EDITOR_H
---
+++ source/tile.cpp
@@ -0,0 +1,95 @@
+#include "tile.h"
+
+/*! Erstellt ein Tile im Koordinatenursprung */
+Tile::Tile() {
+    position = new Point();
+    setFlag(QGraphicsItem::ItemIsMovable);
+    pressed = false;
+    selected = false;
+
+}
+/*! Erstellt ein Tile
+ * \param x x-Position des Tile
+ * \param y y-Position des Tile
+ * \param ascent Steigung des Tile */
+Tile::Tile(double x, double y, double ascent) : ascent(ascent) {
+    position = new Point(x,y);
+    setFlag(QGraphicsItem::ItemIsMovable);
+    pressed = false;
+    selected = false;
+    //setPos(x,y);
+}
+
+/*! Erstellt ein Begrenzungsrechteck für das Tile,Dieses wird sowohl zum zeichnen, als auch für weitere Interaktion benötigt
+ * \return Begrenzungsrechteck für das Tile */
+QRectF Tile::boundingRect() const
+{
+    return QRectF(position->getX(),position->getY(),50,50);
+}
+
+/*! Zeichnet das Tile
+ * \param painter Painter der zum Zeichnen benutzt wird
+ * \param option Optionen für das Zeichnen
+ * \param widget Widget in welches gezeichnet wird */
+void Tile::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    //Nur zum Testen
+    QRectF rec = boundingRect();
+    QBrush brush(Qt::green);
+    painter->fillRect(rec, brush);
+    painter->drawRect(rec);
+}
+
+/*! Gibt die Steigung des Tile zurück
+ * \return Steigung des Tile */
+double Tile::getAscent(){ return ascent; }
+
+/*! Ändert die Position des Tile
+ * \param x neue x-Koordinate des Tile
+ * \param y neue y-Koordinate des Tile */
+void Tile::setPosition(double x, double y)
+{
+    position->setX(x);
+    position->setY(y);
+    //setPos(x,y);
+}
+
+/*! Gibt den Typen des Tile zurück
+ * \return Typ des Tile */
+QString Tile::getType()
+{
+    return "tile";
+}
+
+/*! Ändert die Steigung des Tile
+ * \param nAscent neue Steigung des Tile */
+void Tile::setAscent(double nAscent)
+{
+    ascent = nAscent;
+}
+
+/*! Gibt die Position des Tile zurück
+ * \return Position des Tile */
+Point* Tile::getPosition() { return position; }
+
+/*! Rotiert das Tile */
+void Tile::rotate() {}
+
+/*! Bei Doppelklick wird das Tile rotiert */
+void Tile::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
+{
+    rotate();
+    QGraphicsItem::mouseDoubleClickEvent(event);
+}
+/*! Wählt das angeklickte Tile aus */
+void Tile::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    pressed=true;
+    if(selected){
+       selected = false;
+    } else {
+       selected = true;
+    }
+    update();
+    QGraphicsItem::mousePressEvent(event);
+}
